8ea0a47ac3f4844c49353f4a247a851ff29a6941
noerbarry <noerbarry@gmail.com>
2023-06-03 00:35:13 +0700

    Update deteksi.py

--- deteksi.py
+++ deteksi.py
@@ -7,12 +7,9 @@ import cv2
 # Memuat model deteksi berat sapi
 model_weight = tf.keras.models.load_model('model.h5')
 
-# Memuat model klasifikasi sapi atau bukan
-model_classification = tf.keras.models.load_model('classification_model.h5')
-
-# Fungsi untuk mengubah gambar menjadi array numpy
-def preprocess_image(image):
-    img = image.resize((150, 150))
+# Fungsi untuk mengubah gambar menjadi array numpy dengan ukuran yang ditentukan
+def preprocess_image(image, target_size):
+    img = image.resize(target_size)
     img = img.convert('RGB')  # Mengubah ke skala warna RGB
     img = np.array(img)
     img = img / 255.0
@@ -21,22 +18,19 @@ def preprocess_image(image):
 
 # Fungsi untuk melakukan prediksi berat sapi
 def predict_weight(image):
-    processed_img = preprocess_image(image)
+    processed_img = preprocess_image(image, (150, 150))
     prediction = model_weight.predict(processed_img)[0][0]
     return prediction
 
-# Fungsi untuk melakukan prediksi klasifikasi sapi atau bukan
-def predict_classification(image):
-    processed_img = preprocess_image(image)
-    prediction = model_classification.predict(processed_img)
-    if prediction > 0.5:
-        return "Sapi"
-    else:
-        return "Bukan Sapi"
+# Fungsi untuk konversi berat dari pounds (lbs) ke kilogram (kg)
+def lbs_to_kg(weight_lbs):
+    weight_kg = weight_lbs * 0.453592
+    return round(weight_kg, 2)
 
 # Fungsi untuk membaca frame dari kamera
 def read_camera():
     cap = cv2.VideoCapture(0)  # Mengakses kamera dengan indeks 0
+    button_counter = 0  # Initialize the button counter
     while True:
         ret, frame = cap.read()  # Membaca frame dari kamera
         if not ret:
@@ -48,20 +42,21 @@ def read_camera():
         img = Image.fromarray(frame)
         st.image(img, caption='Kamera Live', use_column_width=True)
         
+        button_counter += 1  # Increment the button counter
+        
         # Memeriksa apakah tombol "Deteksi" ditekan
-        if st.button('Deteksi'):
+        if st.button('Deteksi' + str(button_counter), key='deteksi_button_' + str(button_counter)):
             # Melakukan prediksi berat sapi
-            weight_prediction = predict_weight(img)
+            weight_prediction_lbs = predict_weight(img)
             
-            # Melakukan prediksi klasifikasi sapi atau bukan
-            class_prediction = predict_classification(img)
+            # Konversi berat dari lbs ke kg
+            weight_prediction_kg = lbs_to_kg(weight_prediction_lbs)
             
             # Menampilkan hasil prediksi
-            st.write(f"Prediksi Berat Sapi: {weight_prediction} kg")
-            st.write(f"Prediksi Klasifikasi: {class_prediction}")
-            
-        # Menghentikan streaming video saat tombol 'Stop' ditekan
-        if st.button('Stop'):
+            st.write(f"Prediksi Berat Sapi: {weight_prediction_kg} kg")
+        
+        # Memeriksa apakah tombol "Stop" ditekan
+        if st.button('Stop' + str(button_counter), key='stop_button_' + str(button_counter)):
             break
 
     cap.release()  # Melepaskan kamera
@@ -69,6 +64,15 @@ def read_camera():
 
 # Judul aplikasi web
 st.title("Deteksi Berat dan Klasifikasi Gambar Sapi")
+st.markdown(
+    """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
 
 # Upload gambar
 uploaded_file = st.file_uploader("Pilih gambar", type=["jpg", "jpeg", "png"])
@@ -85,14 +89,15 @@ if uploaded_file is not None:
     # Menampilkan gambar yang diupload
     st.image(image, caption='Gambar', use_column_width=True)
     
+    button_counter = 0  # Initialize the button counter for the uploaded image section
+    
     # Memeriksa apakah tombol "Deteksi" ditekan
-    if st.button('Deteksi'):
+    if st.button('Deteksi' + str(button_counter), key='deteksi_button_' + str(button_counter)):
         # Melakukan prediksi berat sapi
-        weight_prediction = predict_weight(image)
+        weight_prediction_lbs = predict_weight(image)
         
-        # Melakukan prediksi klasifikasi sapi atau bukan
-        class_prediction = predict_classification(image)
+        # Konversi berat dari lbs ke kg
+        weight_prediction_kg = lbs_to_kg(weight_prediction_lbs)
         
         # Menampilkan hasil prediksi
-        st.write(f"Prediksi Berat Sapi: {weight_prediction} kg")
-        st.write(f"Prediksi Klasifikasi: {class_prediction}")
+        st.write(f"Prediksi Berat Sapi: {weight_prediction_kg} kg")
